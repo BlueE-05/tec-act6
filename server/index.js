b4c1cecbe23faa27be0a43980326315998277949
BlueE-05 <danaetorres35@gmail.com>
2025-03-13 14:35:59 -0600

    Actualización server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+const cors = require('cors');
 const express = require('express');
+
 const app = express();
+app.use(cors());
 
 app.get('/calcular/:num1/:operacion/:num2', (req, res) => {
     const num1 = parseFloat(req.params.num1);
